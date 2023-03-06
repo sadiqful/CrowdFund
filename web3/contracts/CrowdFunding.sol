052f53504982cfc7ac0eae54a7226baa2b2845b5
Aliyu <sadiqful@gmail.com>
2023-03-06 19:37:00 +0100

    get campaign donators

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -53,7 +53,9 @@ contract CrowdFunding {
     }
 
     function getDonators(uint256 _id) public view returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
 
-    } 
+    
 
 }
