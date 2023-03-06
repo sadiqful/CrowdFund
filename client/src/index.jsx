e96ee32150003af438959799cb62a1ef8adc0026
Aliyu <sadiqful@gmail.com>
2023-03-06 20:36:31 +0100

    changed to jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router} from 'react-router-dom'
 import { ChainId, ThirdWebProvider } from '@thirdweb-dev/react'
 
+import App from './App'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
