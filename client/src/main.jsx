a2abaae03c4e84477449f51a518bf7be766f4d16
Aliyu <sadiqful@gmail.com>
2023-03-07 13:33:32 +0100

    changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,10 @@
-import react from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router} from 'react-router-dom'
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
+import react from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router} from 'react-router-dom';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
-import App from './App'
+import App from './App';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
